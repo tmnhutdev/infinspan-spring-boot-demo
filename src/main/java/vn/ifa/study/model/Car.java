5d71284fbc86b7a98d0d0a7e10d3f6551299148c
Nhut Thai <nhut.thai@swisspost.com>
2023-02-23 14:43:20 +0700

    add docker-image.yaml

--- src/main/java/vn/ifa/study/model/Car.java
+++ src/main/java/vn/ifa/study/model/Car.java
@@ -1,12 +1,10 @@
 package vn.ifa.study.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-//@AllArgsConstructor
 public class Car {
     
     private String model;
--- src/main/java/vn/ifa/study/model/Owner.java
+++ src/main/java/vn/ifa/study/model/Owner.java
@@ -2,13 +2,11 @@ package vn.ifa.study.model;
 
 import java.util.List;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-//@AllArgsConstructor
 public class Owner {
     
     private String id;

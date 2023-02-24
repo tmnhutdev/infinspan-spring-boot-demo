8e56f1b238c9ad6eda66d31f95ccd7517c4dba6e
Nhut Thai <nhut.thai@swisspost.com>
2023-02-24 15:44:26 +0700

    add NODE_ID properties

--- src/main/java/vn/ifa/study/InfinispanDemoApplication.java
+++ src/main/java/vn/ifa/study/InfinispanDemoApplication.java
@@ -1,12 +1,26 @@
 package vn.ifa.study;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StringUtils;
 
 @SpringBootApplication(scanBasePackages = "vn.ifa.study")
 public class InfinispanDemoApplication {
 
 	public static void main(String[] args) {
+	    String nodeId = System.getProperty("NODE_ID");
+	    if(!StringUtils.hasLength(nodeId)) {
+	        try {
+                nodeId = InetAddress.getLocalHost().getHostName();
+            }
+            catch (UnknownHostException e) {
+                e.printStackTrace();
+            }
+	    }
+	    System.setProperty("NODE_ID", nodeId);
 		SpringApplication.run(InfinispanDemoApplication.class, args);
 	}
 

6fbaa256a25a11fb56316bd1f551446f70a2e9c0
Nhut Thai <nhut.thai@swisspost.com>
2023-02-25 12:19:18 +0700

    update run.sh and version

--- src/main/resources/run.sh
+++ src/main/resources/run.sh
@@ -2,5 +2,4 @@
 
 java -Djboss.default.jgroups.stack=kubernetes \
 -Djgroups.dns.query=infinispan-cluster.develop.svc.cluster.local \
--Dspring.config.location=config/ \
 -jar infinspan-spring-boot-demo.jar

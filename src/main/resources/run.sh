70c9668e01cef6723c0166466eefc7f6ae28a2ca
Nhut Thai <nhut.thai@swisspost.com>
2023-02-26 12:22:12 +0700

    Update external properties

--- src/main/resources/run.sh
+++ src/main/resources/run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-java -Djboss.default.jgroups.stack=kubernetes \
--Djgroups.dns.query=infinispan-cluster.develop.svc.cluster.local \
+java -Dcom.sun.management.jmxremote=true \
+-Dcom.sun.management.jmxremote.port=5000 \
+-Dcom.sun.management.jmxremote.authenticate=false \
+-Dcom.sun.management.jmxremote.ssl=false \
+-Dcom.sun.management.jmxremote.local.only=false \
 -jar infinspan-spring-boot-demo.jar
